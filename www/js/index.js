fca31992d1256f9326881d16e3abffa1c20195cc
Solidux <rurouni.nestor@gmail.com>
2016-01-26 12:43:13 -0600

    ajustes para que no aparezca en la barra

--- www/js/index.js
+++ www/js/index.js
@@ -1,25 +1,16 @@
 var app = {
-    // Application Constructor
     initialize: function() {
         this.bindEvents();
     },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
     },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
     },
-    // Update DOM on a Received Event
     receivedEvent: function(id) {
-		var ref = cordova.InAppBrowser.open('http://apache.org', '_blank', 'location=yes');
+		var ref = cordova.InAppBrowser.open('http://apache.org', '_blank', 'location=no');
+		ref.addEventListener('exit', function(event) { navigator.app.exitApp(); });
     }
 };
 
